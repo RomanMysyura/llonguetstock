2fcdae00103c7eed8817b1ecc18b810c560980d2
Roman Mysyura <romagotanki@gmail.com>
2025-02-18 22:57:45 +0100

    New

--- resources/js/Components/MenuComponents/MagatzemComponents/GeneralStat.vue
+++ resources/js/Components/MenuComponents/MagatzemComponents/GeneralStat.vue
@@ -1,24 +1,36 @@
 <script setup>
-import { Head } from '@inertiajs/vue3';
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const totalCategories = ref(0)
+const totalProducts = ref(0)
+
+onMounted(async () => {
+  const { data } = await axios.get('/api/getallcategories')
+  totalCategories.value = data.length
+  totalProducts.value = data.reduce(
+    (sum, category) => sum + category.products.length,
+    0
+  )
+})
 </script>
 
 <template>
-
     
         <div class="stats stats-vertical lg:stats-horizontal shadow bg-amber-900 text-white w-full mt-3">
             <div class="stat">
                 <div class="stat-title text-white font-bold">Total Categories</div>
-                <div class="stat-value">16</div>
+                <div class="stat-value">{{ totalCategories }}</div>
             </div>
 
             <div class="stat">
                 <div class="stat-title text-white font-bold">Total Productes</div>
-                <div class="stat-value">1532</div>
+                <div class="stat-value">{{ totalProducts }}</div>
             </div>
 
             <div class="stat">
                 <div class="stat-title text-white font-bold">Alertas</div>
-                <div class="stat-value">1,200</div>
+                <div class="stat-value">---</div>
             </div>
         </div>
 
